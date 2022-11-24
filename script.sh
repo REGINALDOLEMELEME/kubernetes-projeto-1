dca0221cca82d0ebca1a6d12d190f55892e23dcf
Reginaldo Leme <mykepmbr@gmail.com>
2022-11-24 00:00:42 -0300

    Criação da pasta database, arquivo sql para gerar o banco de dados, dockerfile, arquivo yml service e deployment e ainda script para execução

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens...."
+
+docker build -t reginaldoleme/projeto-backend:1.0 backend/.
+docker build -t reginaldoleme/database:1.0 database/.
+
+echo "fazendo upload das imagens....."
+
+docker push reginaldoleme/projeto-backend:1.0
+docker push reginaldoleme/database:1.0
+
+echo "Criando serviços no cluster Kubernetes"
+
+kubectl apply -f ./services.yml
+
+echo "Realizandos os Deployments"
+
+kubectl apply -f ./deployments.yml
